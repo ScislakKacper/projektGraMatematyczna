93a72e4721b623dbe648605bd01361ed2a2a4ab3
ScislakKacper <kacpersci7@gmail.com>
2026-03-27 12:07:15 +0100

    Przycisk do zamykania aplikacji i wyswietlania okna z zasadami gry (bez stylu)

--- app/src/main/java/com/kacper/projektgramatematyczna/MainActivity.java
+++ app/src/main/java/com/kacper/projektgramatematyczna/MainActivity.java
@@ -1,15 +1,22 @@
 package com.kacper.projektgramatematyczna;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageButton zamknijApke;
+    private ImageButton infoGra;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,29 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        zamknijApke = findViewById(R.id.zamknij_apke);
+        infoGra = findViewById(R.id.info_gra);
+
+        zamknijApke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+        infoGra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(MainActivity.this, R.style.AlertDialogCustom));
+                builder.setMessage("Zasady gry:\n1. Zasada1\n2. Zasada2\n3. Zasada3");
+                builder.setCancelable(true);
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+                alertDialog.getWindow().setLayout(700, 700);
+            }
         });
     }
 }
