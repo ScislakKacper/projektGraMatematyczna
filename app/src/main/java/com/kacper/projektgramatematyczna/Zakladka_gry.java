c9d93d348768288564cbc34a4872302873a12607
ScislakKacper <kacpersci7@gmail.com>
2026-04-01 02:08:34 +0200

    Wypisanie tresci pierwszego pytania w Toast

--- app/src/main/java/com/kacper/projektgramatematyczna/Zakladka_gry.java
+++ app/src/main/java/com/kacper/projektgramatematyczna/Zakladka_gry.java
@@ -1,6 +1,10 @@
 package com.kacper.projektgramatematyczna;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.OnBackPressedCallback;
@@ -20,6 +24,19 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Zakladka_gry extends AppCompatActivity {
+    List <Pytanie> pytanie;
+    TextView textViewPytanie;
+    RadioGroup radioGroup;
+    int radioButton[] = new int[]{
+            R.id.odpa,
+            R.id.odpb,
+            R.id.odpc,
+            R.id.odpd
+    };
+    RadioButton ButtonOdpA;
+    RadioButton ButtonOdpB;
+    RadioButton ButtonOdpC;
+    RadioButton ButtonOdpD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +48,14 @@ public class Zakladka_gry extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        textViewPytanie = findViewById(R.id.trescPytania);
+        radioGroup = findViewById(R.id.odpowiedzi);
+        ButtonOdpA = findViewById(R.id.odpa);
+        ButtonOdpB = findViewById(R.id.odpb);
+        ButtonOdpC = findViewById(R.id.odpc);
+        ButtonOdpD = findViewById(R.id.odpd);
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://my-json-server.typicode.com/ScislakKacper/projektGraMatematyczna/")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -40,12 +65,17 @@ public class Zakladka_gry extends AppCompatActivity {
         call.enqueue(new Callback<List<Pytanie>>() {
             @Override
             public void onResponse(Call<List<Pytanie>> call, Response<List<Pytanie>> response) {
-
+                if(!response.isSuccessful()){
+                    Toast.makeText(Zakladka_gry.this, response.code(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                List<Pytanie> pytania = response.body();
+                Toast.makeText(Zakladka_gry.this, pytania.get(0).getTrescPytania(), Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onFailure(Call<List<Pytanie>> call, Throwable t) {
-
+                Toast.makeText(Zakladka_gry.this, t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
 
